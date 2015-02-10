420b8ed170f4e2c5fbb4e7711e720638ac06633e
Zoar <zoar@k-side.net>
2015-02-10 15:50:31 +0900

    引数になっているファイル名の拡張子をgeojsonに変更して作成するように変更

--- sima2geojson.rb
+++ sima2geojson.rb
@@ -5,22 +5,12 @@ require 'json'
 require 'kconv'
 
 filename = ARGV[0]
-name = ARGV[1].to_s
-number = ARGV[2].to_s
+number = ARGV[1].to_s
 featuresstock = []
 
-epsg2000 = {"1"=>"2443", "2"=>"2444", "3"=>"2445", "4"=>"2446", "5"=>"2447", "6"=>"2448", "7"=>"2449", "8"=>"2450", "9"=>"2451", "10"=>"2452", "11"=>"2453", "12"=>"2454", "13"=>"2455", "14"=>"2456", "15"=>"2457", "16"=>"2458", "17"=>"2459", "18"=>"2460", "19"=>"2461"}
+epsgcodes = {"1"=>"2443", "2"=>"2444", "3"=>"2445", "4"=>"2446", "5"=>"2447", "6"=>"2448", "7"=>"2449", "8"=>"2450", "9"=>"2451", "10"=>"2452", "11"=>"2453", "12"=>"2454", "13"=>"2455", "14"=>"2456", "15"=>"2457", "16"=>"2458", "17"=>"2459", "18"=>"2460", "19"=>"2461"}
 
-epsg2011 = {"1"=>"6669", "2"=>"6670", "3"=>"6671", "4"=>"6672", "5"=>"6673", "6"=>"6674", "7"=>"6675", "8"=>"6676", "9"=>"6677", "10"=>"6678", "11"=>"6679", "12"=>"6680", "13"=>"6681", "14"=>"6682", "15"=>"6683", "16"=>"6684", "17"=>"6685", "18"=>"6686", "19"=>"6687"}
-
-case name
-when "2000"
-   epsgcode = epsg2000[number]
-when "2011"
-   epsgcode = epsg2011[number]
-else
-   puts "unknown name"
-end
+epsgcode = epsgcodes[number]
 
 # Create CRS Properties
 crsproperties = {"name" => "urn:ogc:def:crs:EPSG::#{epsgcode}"}
@@ -43,5 +33,7 @@ open(filename) do |sima|
    end
 end
 
-puts JSON.generate("type"=> "FeatureCollection", "crs" => crs ,"features" => featuresstock)
-
+geojson = JSON.generate("type"=> "FeatureCollection", "crs" => crs ,"features" => featuresstock)
+open(filename.gsub(".sim",".geojson"),"w") do |json|
+   json.write(geojson)
+end
